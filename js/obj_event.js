a992cdd8a2623acc04fd97a6751a84d328846df6
Ehne <song_ms_kr@hanmail.net>
2018-10-18 00:36:46 +0900

    Update obj_event.js

--- js/obj_event.js
+++ js/obj_event.js
@@ -1,35 +1,20 @@
 $(document).on("click", ".to_main", function() {
   var obj = $(this);
-  var file = obj.attr("data-href");
+  var url = obj.attr("data-url");
   var main = $("#main");
   main.empty();
   $.ajax({
     async : false,
-    url : file,
+    url : url,
     success : function(rs) {
       main.append(rs);
     },
     error : function() {
-      main.append("[File not found : " + file + "]");
+      main.append("[File not found : " + url + "]");
     },
     complete : function() {
-    }
-  });
-}).on("ready", ".loading", function() {
-  console.log(".loading");
-  var obj = $(this);
-  var url = obj.attr("data-url");
-  $.ajax({
-    async : false,
-    url : url,
-    success : function(rs) {
-      obj.after(rs);
-    },
-    error : function() {
-      obj.after("[File not found : " + url + "]");
-    },
-    complete : function() {
-      obj.remove();
+      $("#main_menu a.current").removeClass("current");
+      $("#main_menu a[name=" + url + "]").addClass("current");
     }
   });
 });
