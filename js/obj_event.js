a63a0476176d6690e90d70c9189e551b082a44e7
Ehne <song_ms_kr@hanmail.net>
2018-10-17 21:40:02 +0900

    Update obj_event.js

--- js/obj_event.js
+++ js/obj_event.js
@@ -1,1 +1,18 @@
-
+$.on("click", ".to_main", function() {
+  var obj = $(this);
+  var file = obj.attr("data-href");
+  var main = $("#main");
+  main.empty();
+  $.ajax({
+    async : false,
+    url : file,
+    success : function(rs) {
+      main.append(rs);
+    },
+    error : function() {
+      main.append("[File not found : " + file + "]");
+    },
+    complete : function() {
+    }
+  });
+});
