ac7fbea2dd16481a96e00aed3525fde32962bea3
Ehne <song_ms_kr@hanmail.net>
2018-10-18 01:20:13 +0900

    Update obj_event.js

--- js/obj_event.js
+++ js/obj_event.js
@@ -2,15 +2,15 @@ $(document).on("click", ".to_main", function() {
   var obj = $(this);
   var url = obj.attr("data-url");
   var main = $("#main");
-  main.empty();
+  main.empty().append("Loading...Loading...Loading...Loading...Loading...Loading...Loading...Loading...").append("<div style=text-align=center;font-size:3em>Loading...</div>");;
   $.ajax({
     async : false,
     url : url,
     success : function(rs) {
-      main.append(rs);
+      main.empty().append(rs);
     },
     error : function() {
-      main.append("[File not found : " + url + "]");
+      main.empty().append("[File not found : " + url + "]");
     },
     complete : function() {
       $("#main_menu a.current").removeClass("current");
