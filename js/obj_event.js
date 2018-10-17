21ce1e295bfbc4884b4706c5fc6aac5b63ea487a
Ehne <song_ms_kr@hanmail.net>
2018-10-17 22:24:06 +0900

    Update obj_event.js

--- js/obj_event.js
+++ js/obj_event.js
@@ -15,4 +15,20 @@ $(document).on("click", ".to_main", function() {
     complete : function() {
     }
   });
+}).on("ready", ".loading", function() {
+  var obj = $(this);
+  var url = obj.attr("data-url");
+  $.ajax({
+    async : false,
+    url : url,
+    success : function(rs) {
+      obj.after(rs);
+    },
+    error : function() {
+      obj.after("[File not found : " + url + "]");
+    },
+    complete : function() {
+      obj.remove();
+    }
+  });
 });
