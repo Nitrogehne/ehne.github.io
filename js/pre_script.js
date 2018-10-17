b453a7404008d5670f9cd2df96ae020c45165017
Ehne <song_ms_kr@hanmail.net>
2018-10-17 22:47:48 +0900

    Update pre_script.js

--- js/pre_script.js
+++ js/pre_script.js
@@ -4,33 +4,33 @@ $(function() {
 
 function load_file() {
   var count = $("element").length;
-  $(".file").each(function(i) {
+  $(".load").each(function(i) {
     var obj = $(this);
-    var file = obj.attr("data-file");
+    var url = obj.attr("data-url");
     $.ajax({
       async : false,
-      url : file,
+      url : url,
       success : function(rs) {
         obj.after(rs);
       },
       error : function() {
-        obj.after("[File not found : " + file + "]");
+        obj.after("[File not found : " + url + "]");
       },
       complete : function() {
         obj.remove();  
         if (i + 1 === count) {
             
           //load_file();
-            console.log("complete", $(".file"), $(".file").length);
+            console.log("complete", $(".load"), $(".load").length);
         }
       }
     });
   }).promise().done(function() {    
       console.log("promise");
-    if($(".file").length) {          
+    if($(".load").length) {          
         if (i + 1 === count) {
           //load_file();
-            console.log($(".file"), $(".file").length);
+            console.log($(".load"), $(".load").length);
         }
     }
   });
