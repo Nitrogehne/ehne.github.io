f1ac0df3a0299aa1d2993867ff9d29076c59bc12
Ehne <song_ms_kr@hanmail.net>
2018-10-19 17:44:17 +0900

    Update obj_event.js

--- js/obj_event.js
+++ js/obj_event.js
@@ -10,11 +10,18 @@ $(document).on("click", ".to_main", function() {
       main.append(rs);
     },
     error : function() {
-      main.append("[File not found : " + url + "]");
+      main.append("[ File not found ]");
+      console.log("[File not found : " + url + "]");
     },
     complete : function() {
       $("#main_menu a.current").removeClass("current");
-      $("#main_menu a[data-url='" + url + "']").addClass("current");
+      var current = $("#main_menu a[data-url='" + url + "']");
+      var parent = current.parent().parent().parent();
+      if(parent.attr("id") == "main_menu"){
+        current.addClass("current");
+      } else {
+        parent.children("a").addClass("current");
+      }
     }
   });
 });
